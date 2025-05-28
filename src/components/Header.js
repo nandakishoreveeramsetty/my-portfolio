f788237835474875bd7e1bba4ffa69ccbb3131ae
Nanda <nandakishore3393@gmail.com>
2025-05-28 22:45:06 +0530

    added the Pic in the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,27 +4,35 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="name">Nanda Kishore Veeramsetty</h1>
-      <h3 className="title">CHATBOTS / CCAI / GCP - GEN AI DEVELOPER</h3>
+      <div className="header-text">
+        <h1 className="name">Nanda Kishore Veeramsetty</h1>
+        <h3 className="title">CHATBOTS / CCAI / GCP - GEN AI DEVELOPER</h3>
 
-      <p className="contact">
-        📍 Bangalore | 📞 +91 9618821719 | 📧{" "}
-        <a href="mailto:nandakishore3393@gmail.com" className="link">
-          nandakishore3393@gmail.com
-        </a>
-      </p>
+        <p className="contact">
+          📍 Bangalore | 📞 +91 9618821719 | 📧{" "}
+          <a href="mailto:nandakishore3393@gmail.com" className="link">
+            nandakishore3393@gmail.com
+          </a>
+        </p>
 
-      <p>
-        🔗{" "}
-        <a
-          href="https://www.linkedin.com/in/nandakishore3/"
-          target="_blank"
-          rel="noreferrer"
-          className="link"
-        >
-          LinkedIn Profile
-        </a>
-      </p>
+        <p>
+          🔗{" "}
+          <a
+            href="https://www.linkedin.com/in/nandakishore3/"
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            LinkedIn Profile
+          </a>
+        </p>
+      </div>
+
+      <img
+        src={`${process.env.PUBLIC_URL}/Nanda.jpg`}
+        alt="Profile"
+        className="profile-pic"
+      />
     </header>
   );
 };
