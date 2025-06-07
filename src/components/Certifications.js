22bd2b4268c37b07eed6d4c9162fc1d039833f98
Nanda <nandakishore3393@gmail.com>
2025-06-08 01:44:59 +0530

    added more certs

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -23,6 +23,56 @@ const certifications = [
     credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/5939441?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
   },
   {
+    title: "Deploy Multi-Agent Systems with Agent Development Kit and Agent Engine",
+    date: "",
+    credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/15685208?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Build generative virtual agents with API integrations",
+    date: "",
+    credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/13567829?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Build generative virtual agents with API integrations - Advanced Skill Badge",
+    date: "",
+    credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/13567829?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Building Complex Self-Service Experiences in Conversational Agents",
+    date: "",
+    credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/13550822?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Incorporating Generative Features into Complex Conversational Agents",
+    date: "",
+    credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/13567726?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Intro to Conversational AI and Conversational AI Engagement Framework",
+    date: "",
+    credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/13378455?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Advanced Webhook Concepts",
+    date: "",
+    credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/9674284?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "Conversational AI Telephone and Chat Integrations",
+    date: "",
+    credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/9669272?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+  },
+  {
+    title: "DialofFlow CX: Managing Environments",
+    date: "",
+    credential: "https://www.coursera.org/account/accomplishments/records/X3CGG32Z87YW",
+  },
+  {
+    title: "DialofFlow CX: Parameter Manupulation",
+    date: "",
+    credential: "https://www.coursera.org/account/accomplishments/records/L22GATJ76ULY",
+  },
+  {
     title: "Generative AI Fundamentals, Google",
     date: "",
     credential: "https://partner.cloudskillsboost.google/public_profiles/72534f49-272a-4d93-8f01-7f32e475b92a/badges/4107486?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
@@ -32,6 +82,7 @@ const certifications = [
     date: "",
     credential: "https://app.pluralsight.com/achievements/share/17723c64-a150-4c6a-9db7-5e5bb0d2539e",
   },
+  
 ];
 
 const Certifications = () => {
